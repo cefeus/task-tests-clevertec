85f3f6cb5b04a6f0885f343ec9cca82a8df1125d
cefe!s <fireinspirer@gmail.com>
2023-11-04 14:17:24 +0300

    feat/ implementation

--- src/main/java/ru/clevertec/product/mapper/ProductMapper.java
+++ src/main/java/ru/clevertec/product/mapper/ProductMapper.java
@@ -1,9 +1,13 @@
 package ru.clevertec.product.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import ru.clevertec.product.data.InfoProductDto;
 import ru.clevertec.product.data.ProductDto;
 import ru.clevertec.product.entity.Product;
 
+@Mapper
 public interface ProductMapper {
 
     /**
@@ -12,6 +16,8 @@ public interface ProductMapper {
      * @param productDto - DTO для маппинга
      * @return новый продукт
      */
+    @Mapping(target = "uuid", ignore = true)
+    @Mapping(target = "created", ignore = true)
     Product toProduct(ProductDto productDto);
 
     /**
@@ -30,5 +36,7 @@ public interface ProductMapper {
      * @param productDto информация для обновления
      * @return обновлённый продукт
      */
-    Product merge(Product product, ProductDto productDto);
+    @Mapping(target = "uuid", ignore = true)
+    @Mapping(target = "created", ignore = true)
+    Product merge(@MappingTarget Product product, ProductDto productDto);
 }
--- src/main/java/ru/clevertec/product/service/impl/ProductServiceImpl.java
+++ src/main/java/ru/clevertec/product/service/impl/ProductServiceImpl.java
@@ -3,12 +3,17 @@ package ru.clevertec.product.service.impl;
 import lombok.RequiredArgsConstructor;
 import ru.clevertec.product.data.InfoProductDto;
 import ru.clevertec.product.data.ProductDto;
+import ru.clevertec.product.entity.Product;
+import ru.clevertec.product.exception.ProductNotFoundException;
 import ru.clevertec.product.mapper.ProductMapper;
 import ru.clevertec.product.repository.ProductRepository;
 import ru.clevertec.product.service.ProductService;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
@@ -18,26 +23,46 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public InfoProductDto get(UUID uuid) {
-        return null;
+        Optional<Product> retrievedProduct = productRepository.findById(uuid);
+
+        if (retrievedProduct.isEmpty())
+            throw new ProductNotFoundException(uuid);
+
+        return mapper.toInfoProductDto(retrievedProduct.get());
     }
 
     @Override
     public List<InfoProductDto> getAll() {
-        return null;
+        List<Product> productList = productRepository.findAll();
+        return productList.stream()
+                .map(product -> mapper.toInfoProductDto(product))
+                .collect(Collectors.toList());
     }
 
     @Override
     public UUID create(ProductDto productDto) {
-        return null;
+        Product productToCreate = mapper.toProduct(productDto);
+        return productRepository.save(productToCreate).getUuid();
     }
 
     @Override
     public void update(UUID uuid, ProductDto productDto) {
+        Product productToUpdate = mapper.toProduct(productDto);
+        productToUpdate.setUuid(uuid);
 
+        try {
+            productRepository.save(productToUpdate);
+        } catch (NoSuchElementException e) {
+            throw new ProductNotFoundException(uuid);
+        }
     }
 
     @Override
     public void delete(UUID uuid) {
-
+        try {
+            productRepository.delete(uuid);
+        } catch (NoSuchElementException e) {
+            throw new ProductNotFoundException(uuid);
+        }
     }
 }
